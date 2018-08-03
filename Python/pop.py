a086b742705b66b34710e8170646cf87c50cf2f3
Harvey <34975277+HewieWang@users.noreply.github.com>
2018-08-03 13:13:49 +0800

    Add files via upload
    selenium识别alert并确定

---
+++ Python/pop.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('http://www.w3school.com.cn/tiy/loadtext.asp?f=hdom_alert')
+driver.find_element_by_xpath('/html/body/input').click()
+a1 = driver.switch_to.alert
+a1.accept()
