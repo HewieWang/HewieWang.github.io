5c71911a0ea7584b4959a97b8b9dbe0d939aafd4
Harvey <34975277+HewieWang@users.noreply.github.com>
2018-08-25 20:53:17 +0800

    仿TP链式操作

--- PHP/db.php
+++ PHP/db.php
@@ -255,3 +255,19 @@ class Db{
     }
  
 }
+
+
+// 使用
+
+$db = new Db();
+//查询操作
+var_dump($db->table('user')->where('id > 2')->order('id desc')->limit('2,4')->select());
+ 
+//插入操作
+var_dump($db->table('user')->insert(array('username'=>'user','password'=>'pwd')));
+ 
+//更新操作
+var_dump($db->table('user')->where('id = 1')->update(array('username'=>'user1','password'=>'pwd1')));
+ 
+//删除操作
+var_dump($db->table('user')->where('id = 1')->delete());
