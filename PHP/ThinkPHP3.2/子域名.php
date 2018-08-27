7fad3e4d946fafc6ac8c941fb9893d4b899d6248
Harvey <34975277+HewieWang@users.noreply.github.com>
2018-08-27 21:34:52 +0800

    add

---
+++ PHP/ThinkPHP3.2/子域名.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'APP_SUB_DOMAIN_DEPLOY'   =>    1, // 开启子域名配置
+	'APP_SUB_DOMAIN_RULES'    =>    array(   
+	    'ht.bjy.test'  => 'Admin',
+	    'test.domain2.com'   => 'Test',
+	),
+);
