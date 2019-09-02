c5654398c03b35608497a3b8bf724cc89d696aef
Harvey <34975277+HewieWang@users.noreply.github.com>
2019-09-02 16:43:18 +0800

    Tkinter简单全屏应用

---
+++ Python/full_screen.py
@@ -0,0 +1,30 @@
+import tkinter
+from tkinter.messagebox import *
+import sys
+win = tkinter.Tk()
+w = win.winfo_screenwidth()
+h = win.winfo_screenheight()
+win.geometry("%dx%d" %(w,h))
+win.resizable(width=False, height=True)
+win.overrideredirect(True)
+win.attributes("-topmost",True)
+label=tkinter.Label(win,text="Hello Python",font=('宋体',50))
+label2=tkinter.Label(win,text="What u want?",font=('宋体',50))
+label.pack()
+label2.pack()
+T=tkinter.Entry(width=50,font=('宋体',50)) #创建文本框
+T.pack()
+# T = tkinter.Text(win, height=10, width=50,font=('宋体',50))
+# T.pack()
+def getMe():
+	ps=T.get()
+	if ps=="123456":
+		showinfo(title='Hello', message="SUCCESS")
+		sys.exit()
+		pass
+	else:
+		showinfo(title='Hello', message="失败")
+	pass
+b = tkinter.Button(win, height=2, width=9,font=('宋体',40),text='确定',command=getMe)
+b.pack()
+win.mainloop()
